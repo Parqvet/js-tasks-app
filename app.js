306e45b4b602eacbe628f01263cfa5f9f445065a
Marcelo <marcelosen4@gmail.com>
2020-10-13 08:07:18 -0300

    agregado logica condicional para almacenar y actualizar tareas en el localStorage

--- app.js
+++ app.js
@@ -12,15 +12,27 @@ function saveTask(e) {
         description
     };
 
-    // Con el metodo setItem guardo los valores del objeto en el localStorage
-    //Luego con el metodo stringify del objeto JSON convierto al objeto en cadena de texto
-    localStorage.setItem('tasks', JSON.stringify(task));
-    //Obtengo por consola la clave 'tasks' guardada en el localStorage con el metodo getItem
-    console.log(localStorage.getItem('tasks'));
-    // Con el metodo parse puedo volver a pasar a objeto la clave 'tasks' que habia transformado en string
-    // Luego a estos objetos los voy a almacenar en un array
-    console.log(JSON.parse(localStorage.getItem('tasks')));
-    console.log(task.description);
+    // Si desde localStorage ya existe una clave llamada 'tasks' y es igual a nulo, vamos a empezar a crear tareas
+    if (localStorage.getItem('tasks') === null) {
+        //Creo un arreglo llamado tasksLocals y lo dejo vacio para empezar a guardar tareas
+        let tasksLocals = [];
+        // A este arreglo lo vamos llenando de tareas nuevas con el metodo push
+        // Las tareas obtenidas son del objeto task
+        tasksLocals.push(task);
+
+        // Una vez obtenido este arreglo lo almaceno en el localStorage
+        // Lo almaceno en formato string
+        localStorage.setItem('tasks', JSON.stringify(tasksLocals));
+
+    } else {
+    // Caso contrario si ya existen valores alli vamos a empezar a actualizarlos
+    // Obtengo las tareas y las almaceno en una constante convertidas en formato Json.parse
+        const newTasks = JSON.parse(localStorage.getItem('tasksLocals'));
+        // Ahora empiezo a actualizarlas con el metodo push
+        newTasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(newTasks));
+
+    }
 
     e.preventDefault();
 }
